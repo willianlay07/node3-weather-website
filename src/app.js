c8ef2293d6b3fafc4256fff8eaa0f743054374a2
Wai Hlyan Hein <wai@Wais-MacBook-Pro.local>
2023-01-14 23:07:02 +0800

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ const forecast  = require('./utils/forecast')
 //console.log(path.join(__dirname, '../public'))
 
 const app       = express()
+const port      = process.env.PORT || 3000
 
 // Define paths for Express config
 const publicDirectoryPath   = path.join(__dirname, '../public')
@@ -147,6 +148,6 @@ app.get('*', (req, res) => {
 })
 
 // Start the server
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
 })
